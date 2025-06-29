8db6f3f21b2e1ee226e3966153975d46f87a6c1f
abrahamyyc <abraham.yyc@gmail.com>
2025-06-29 14:46:38 -0700

    C: groups added to main

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
+    player = Player(x, y, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        updateable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
         
 if __name__ == "__main__":
     main()
